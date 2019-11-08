cfadbb15e5f22a26ad54167df02514687b9643c4
rosehipdan12 <51744476+rosehipdan12@users.noreply.github.com>
2019-11-08 22:21:44 +0700

    Edit template.

---
+++ Web/Webtest2/Webtest2/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Webtest2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            PSSEntities ps = new PSSEntities();
+            var item = (from e in ps.categories select e).ToList();
+    
+            return View(item);
+        }
+
+    }
+}
---
+++ Web/Webtest2/Webtest2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Webtest2.MvcApplication" Language="C#" %>
