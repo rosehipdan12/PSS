f9a8044c0f0b2dc233bf05e15e10fd0c1815193e
Duc An <ducanle1204@gmail.com>
2019-11-09 11:14:31 +0700

    Revert "update 2.0"
    This reverts commit 0ac52e1ba9c192624419f090ec0a2b0d4bdbe933.

---
+++ Web/Webtest2/Webtest2/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PSSWeb.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public String Email { get; set; }
+    }
+}
