90543a7dc119c20143459ec4082f2be5336ed9d7
rosehipdan12 <51744476+rosehipdan12@users.noreply.github.com>
2019-11-09 11:34:35 +0700

    Revert "Merge branch 'master' of https://github.com/rosehipdan12/PSS"
    This reverts commit 0b3f9eba5220f540cf03fdeacd7dd1e80733c131, reversing
    changes made to 0ffb611f3c5c8d0c5e1384bc18f51dcc80e29059.

--- Web/Webtest2/Webtest2/App_Start/RouteConfig.cs
+++ Web/Webtest2/Webtest2/App_Start/RouteConfig.cs
@@ -10,15 +10,10 @@ namespace Webtest2
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Friend", "Friend/{name}", new{
-                    controller = "Friend", action = "Search", name = UrlParameter.Optional
-                });
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-            
         }
     }
 }
