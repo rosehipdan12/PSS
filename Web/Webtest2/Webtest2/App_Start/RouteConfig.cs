da14740b08de35e219a93dd7a12a9c789da8412a
rosehipdan12 <51744476+rosehipdan12@users.noreply.github.com>
2019-11-11 02:03:17 +0700

    web update

--- Web/Webtest2/Webtest2/App_Start/RouteConfig.cs
+++ Web/Webtest2/Webtest2/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Webtest2
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Cart",
+                url: "{controller}/{action}/{id}/{product}",
+                defaults: new { controller = "Home", action = "Buy", id = UrlParameter.Optional,product=UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ Web/Webtest2/Webtest2/cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webtest2
+{
+    public class cart
+    {
+        public item item;
+        public pet pet;
+        public int quantity;
+    }
+}
