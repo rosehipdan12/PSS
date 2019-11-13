e5e8c13d75d9621918a16f7ffde98d607208d366
Duc An <ducanle1204@gmail.com>
2019-11-13 23:16:48 +0700

    commit form login chưa hoàn thành

---
+++ PSS_WINFORM_CONTROL/PSS_WINFORM_CONTROL/LoginForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace PSS_WINFORM_CONTROL
+{
+    public partial class LoginForm : DevExpress.XtraEditors.XtraForm
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
